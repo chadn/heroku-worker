443e2ff2913240849cf40486bacc19b72d84cece
Chad Norwood <chad@chadnorwood.com>
2013-04-06 16:40:15 -0500

    update shayna's commute to post to api-sails

--- conf/commute.shayna.js
+++ conf/commute.shayna.js
@@ -4,31 +4,57 @@ var stathat_email = require('system').env.STATHAT_EMAIL || '____your____@__email
 var stathatBase = 'https://api.stathat.com/ez?email=' + stathat_email;
 
 var urls = [{
+	name: 'To Shayna Studio',
 	route: 'https://maps.google.com/maps?hl=en'
 	  + '&saddr=2733+N+Troy+St,+Chicago,+IL'
 	  + '&daddr=2150+S+Canalport+Ave,+Chicago,+IL+60608,+USA',
-	urlSuccess: stathatBase + '&stat=To+Shayna+Studio&value=',
-	urlError: stathatBase + '&stat=To+Shayna+Studio+Errors&count=1'
+	urlSuccess: [
+		stathatBase + '&stat=To+Shayna+Studio&value=',
+		'http://api-sails.herokuapp.com/driving_time/create?t=To-Shayna-Studio&mins='
+	],
+	urlError: [
+		stathatBase + "&stat=To+Shayna+Studio+Errors&count=1&",
+		'http://api-sails.herokuapp.com/driving_time/create?t=To-Shayna-Studio-Error&error=error&note='
+	]
 },{
+	name: 'From Shayna Studio',
 	route: 'https://maps.google.com/maps?hl=en'
 	  + '&daddr=2733+N+Troy+St,+Chicago,+IL'
 	  + '&saddr=2150+S+Canalport+Ave,+Chicago,+IL+60608,+USA',
-	urlSuccess: stathatBase + '&stat=From+Shayna+Studio&value=',
-	urlError: stathatBase + '&stat=From+Shayna+Studio+Errors&count=1'
-
-
+	urlSuccess: [
+		stathatBase + '&stat=From+Shayna+Studio&value=',
+		'http://api-sails.herokuapp.com/driving_time/create?t=From-Shayna-Studio&mins='
+	],
+	urlError: [
+		stathatBase + "&stat=From+Shayna+Studio+Errors&count=1&",
+		'http://api-sails.herokuapp.com/driving_time/create?t=From-Shayna-Studio-Error&error=error&note='
+	]
 },{
+	name: 'To Optics Planet',
 	route: 'https://maps.google.com/maps?hl=en'
 	  + '&saddr=2733+N+Troy+St,+Chicago,+IL'
 	  + '&daddr=3150+Commercial+Ave,+Northbrook,+IL+60608,+USA',
-	urlSuccess: stathatBase + '&stat=To+Optics+Planet&value=',
-	urlError: stathatBase + '&stat=To+Optics+Planet+Errors&count=1'
+	urlSuccess: [
+		stathatBase + '&stat=To+Optics+Planet&value=',
+		'http://api-sails.herokuapp.com/driving_time/create?t=To-Optics-Planet&mins='
+	],
+	urlError: [
+		stathatBase + "&stat=To+Optics+Planet+Errors&count=1&",
+		'http://api-sails.herokuapp.com/driving_time/create?t=To-Optics-Planet-Error&error=error&note='
+	]
 },{
+	name: 'From Optics Planet',
 	route: 'https://maps.google.com/maps?hl=en'
 	  + '&daddr=2733+N+Troy+St,+Chicago,+IL'
 	  + '&saddr=3150+Commercial+Ave,+Northbrook,+IL+60608,+USA',
-	urlSuccess: stathatBase + '&stat=From+Optics+Planet&value=',
-	urlError: stathatBase + '&stat=From+Optics+Planet+Errors&count=1'
+	urlSuccess: [
+		stathatBase + '&stat=From+Optics+Planet&value=',
+		'http://api-sails.herokuapp.com/driving_time/create?t=From-Optics-Planet&mins='
+	],
+	urlError: [
+		stathatBase + "&stat=From+Optics+Planet+Errors&count=1&",
+		'http://api-sails.herokuapp.com/driving_time/create?t=From-Optics-Planet-Error&error=error&note='
+	]
 }];
 
 
